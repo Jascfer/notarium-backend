e65159e97a36f059b2156ea2b9065ec8954de18d
Jascfer <ozgurxspeaktr@gmail.com>
2025-07-08 02:34:18 +0300

    MongoDB bağlantısı eklendi ve production için iyileştirmeler

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const cors = require('cors');
 const { Server } = require('socket.io');
 const session = require('express-session');
 const passport = require('./config/passport');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:YvFJGbyNxePZwHwdgsgBvObpeRVpdhkr@shuttle.proxy.rlwy.net:14555', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB bağlantısı başarılı!'))
+.catch(err => console.error('MongoDB bağlantı hatası:', err));
 
 const app = express();
 app.use(cors());
