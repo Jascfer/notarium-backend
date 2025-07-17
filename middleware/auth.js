a39920da04ac3150583047933df7f950d5ad2b2b
Jascfer <ozgurxspeaktr@gmail.com>
2025-07-17 07:15:00 +0300

    final

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,10 @@
 // jwt middleware
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ message: 'Giriş yapmanız gerekiyor.' });
+}
+
+module.exports = { ensureAuthenticated };
